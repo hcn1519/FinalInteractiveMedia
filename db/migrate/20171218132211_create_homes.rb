e95811d1fa16d6248a50ea8b74eea5ba1fa03946
Hong, Chang Nam <hcn1519@gmail.com>
2017-12-19 19:30:44 +0900

    1219 Update

--- db/migrate/20171218132211_create_homes.rb
+++ db/migrate/20171218132211_create_homes.rb
@@ -2,7 +2,7 @@ class CreateHomes < ActiveRecord::Migration
   def change
     create_table :homes do |t|
       t.string :content
-      t.integer :tag
+      t.string :tag
       t.datetime :writeDate
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20171218132211) do
 
   create_table "homes", force: :cascade do |t|
     t.string   "content"
-    t.integer  "tag"
+    t.string   "tag"
     t.datetime "writeDate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
