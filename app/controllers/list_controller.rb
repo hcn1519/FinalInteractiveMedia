45bf8bc292a0b68fc4ed50b660354cd8920a85b1
Hong, Chang Nam <hcn1519@gmail.com>
2017-12-20 16:53:07 +0900

    final

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -5,19 +5,9 @@ class ListController < ApplicationController
     day = params[:day]
     @startTime = DateTime.strptime("#{month}-#{day}-#{year} 00:00:00.000","%m-%d-%Y %H:%M:%S").to_time
     @endTime = DateTime.strptime("#{month}-#{day}-#{year} 23:59:59.999","%m-%d-%Y %H:%M:%S").to_time
-    puts @time
+
     @targetDate = @startTime
     @homes = Home.where(writeDate: @startTime...@endTime)
-
-    puts @homes
-    # @targetDate = Time.zone.today
-    # @homes = nil
-    # if @time != nil
-    #     @targetDate = @time
-    #     @homes = Home.where(writeDate: @targetDate.beginning_of_day...@targetDate.end_of_day)
-    # else
-    #     @homes = Home.where(writeDate: Time.zone.today.beginning_of_day...Time.zone.today.end_of_day)
-    # end
 
     @isEmpty = false
     if @homes.count == 0
